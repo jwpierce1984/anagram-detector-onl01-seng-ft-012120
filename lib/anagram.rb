01521a33b81dbcb84ead15c9544e921d6e7792e8
Justin Pierce <jwpierce1984@gmail.com>
2020-02-03 19:05:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,4 @@
 class Anagram
-  attr_accessor :collect
   
-  def initialize (word)
-    @collect = word
-  end
   
-  def match (array)
-    array.select {|x| x.split("").sort == @collect.split("").sort}
-  end
 end
