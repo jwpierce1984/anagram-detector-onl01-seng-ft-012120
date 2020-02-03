dee0ed84b7572779805dac034aa84fca29b64517
Justin Pierce <jwpierce1984@gmail.com>
2020-02-03 19:11:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,15 @@
 #%w(enlists google inlets banana)
 
 class Anagram
+  attr_accessor :word, 
+  @@anagram_check = ["enlists", "google", "inlets", "banana"]
   
+  def initialize(word)
+    @word = word
+  end
+  
+  def match
+    
+  end
   
 end
